632da214ed4b81785ff0b891159eb80256563b4a
h2o <h2o_zs@163.com>
2018-09-23 09:16:08 +0000

    Finish layout routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>@yield('title','Laravel练习i')</title>
+		<title>@yield('title','Laravel练习')</title>
+		<link rel="stylesheet" href="/css/app.css" type="text/css">
 	</head>
 	<body>
-		@yield('content')
+		@include('layouts._header')
+
+		<div class="container">
+			<div class="col-md-offset-1 col-md-10">
+				@yield('content')
+				@include('layouts._footer')
+			</div>
+		</div>
+		
 	</body>
 </html>
